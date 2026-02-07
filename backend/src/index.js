f9ad4208e1b64432d0632bdeaa954329ae4743b1
DaniloSantos284 <daniloribeiro761@gmail.com>
2026-02-07 14:25:49 -0300

    Removendo a pasta backend da branch main

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const PORT = process.env.PORT;
-
-// INICIALIZAÇÃO: Inicia servidor Express ouvindo na porta definida em .env ou 4000
-app.listen(PORT, () => {
-  console.log(`API rodando na porta ${PORT}`);
-});
